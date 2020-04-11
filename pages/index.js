5fbaffd63e5ee25bfc102827262d02687d3b4f8f
eastflag <eastflag@gmail.com>
2020-04-11 21:02:50 +0900

    Link 추가

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,10 @@
-export default () => <div>Welcome to next.js!</div>
+import Link from "next/link";
+import React from "react";
+
+export default () =>
+  <div>
+    <Link href="/product">Product</Link>
+    <div>
+      Welcome to next.js!
+    </div>
+  </div>
