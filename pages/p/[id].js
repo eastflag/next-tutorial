84735dbae1cb0f223b9efbc128240a2191431c34
eastflag <eastflag@gmail.com>
2020-04-12 17:15:27 +0900

    동적 라우팅 - 클라이언트 렌더링

---
+++ pages/p/[id].js
@@ -0,0 +1,22 @@
+import Layout from '../../components/MyLayout';
+import axios from 'axios';
+
+const Post = props => (
+  <Layout>
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
+    {props.show.image ? <img src={props.show.image.medium} /> : null}
+  </Layout>
+);
+
+Post.getInitialProps = async function(context) {
+  const { id } = context.query;
+  const res = await axios.get(`https://api.tvmaze.com/shows/${id}`);
+  const show = res.data;
+
+  console.log(`Fetched show: ${show.name}`);
+
+  return { show };
+};
+
+export default Post;
